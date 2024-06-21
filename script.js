19f4b0a8560d8a9f816af9e38991ffe28e0649d4
Rarosuo Hope <165177867+RarosuoHope@users.noreply.github.com>
2024-06-21 13:09:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ document.addEventListener('DOMContentLoaded', function() {
     sections.forEach((section) => {
         section.addEventListener('click', function() {
             const expandElement = section.querySelector('.expand');
+             const  headerOne = section.querySelector('.highlight');
             const toggleIcon = section.querySelector('.toggle-icon-1');
-            const  headerOne = section.querySelector('.highlight')
+           
             if (expandElement.style.display === 'block' || expandElement.style.display === '') {
                 expandElement.style.display = 'none';
                  headerOne.style.backgroundColor = 'initial';
-                 highlight.style.color = 'initial';
+                  headerOne.style.color = 'initial';
                 toggleIcon.classList.remove('rotate');
                
             } else {
